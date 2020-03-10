a577b77c93b6fd209af662844d7a44d08533af18
b216014 <b216014@iiit-bh.ac.in>
2020-03-10 09:09:49 +0530

    Added test to check if the getAllProducts helper fetches all the data or not.

--- tests/helpers/loadAllProducts.test.js
+++ tests/helpers/loadAllProducts.test.js
@@ -1,7 +1,13 @@
 const axios = require('axios');
 const db = require('../../models/index');
+const dbOperations = require('../../src/helpers/dbOperations');
 
-describe('The loadAllProducts helper function ', () => {
 
-
+describe('The getAllProducts helper  ', () => {
+  it('should get all the products from the databse', async () => {
+    const mockDb = jest.spyOn(db.allproducts, 'findAll');
+    mockDb.mockResolvedValue([]);
+    await dbOperations.getAllProducts();
+    expect(mockDb).toHaveBeenCalled();
+  });
 });
